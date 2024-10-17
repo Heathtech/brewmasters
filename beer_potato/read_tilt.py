200772fd785178ab47f203e240483eede32bca50
Joseph McCartney <mccartneyjwh@gmail.com>
2024-10-16 22:14:30 -0500

    logging to *local* db

--- beer_potato/read_tilt.py
+++ beer_potato/read_tilt.py
@@ -6,11 +6,15 @@ import json
 import aioblescan as aiobs
 from aioblescan.plugins import Tilt
 import datetime
+import requests
+import os
+import tomllib
 
 # global
 opts = None
 decoders = []
 tick = datetime.datetime.now()
+secrets = None
 
 
 def my_process(data):
@@ -23,10 +27,14 @@ def my_process(data):
     if decoders:
         for leader, decoder in decoders:
             xx = decoder.decode(ev)
-            if xx and (datetime.datetime.now() - tick).seconds > 15:
-                tick = datetime.datetime.now()
-                print(json.dumps(xx))
-                print("saving to file")
+            if xx and datetime.datetime.now() >= tick:
+                tick = datetime.datetime.now() + datetime.timedelta(seconds=15)
+                heaths_super_secert_url = secrets["server"]["url"]
+                # TODO: need to add the time?= to the url
+                # Saving data to database (or is it???)
+                r = requests.post(heaths_super_secert_url, json=json.loads(xx))
+                print(r)
+                # Local logging for safety
                 with open("massive_debt.json", "+a") as f:
                     now = datetime.datetime.now()
                     f.write(f"{now},")
@@ -70,6 +78,11 @@ async def amain(args=None):
 
 
 def main():
+    # read the secrets file to get access to the top secret server that stores our top secret data
+    global secrets
+    with open("secrets.toml", "rb") as f:
+        secrets = tomllib.load(f)
+
     # Configured to only use the tilt
     decoders.append(("Tilt", Tilt()))
 
