ca287b1dbe5c7425ff4e4f4004d6624b6deed26d
Heathtech <heathbar1989@hotmail.com>
2024-10-21 17:20:19 -0500

    Downgrade spring

--- tilt-webservice/src/main/kotlin/org/heathtech/tilt/webserver/ServletInitializer.kt
+++
@@ -1,9 +0,0 @@
-package org.heathtech.tilt.webserver
-
-import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-
-class ServletInitializer : SpringBootServletInitializer() {
-    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
-        application.sources(TiltWebserverApplication::class.java)
-}
--- tilt-webservice/src/main/kotlin/org/heathtech/tilt/webserver/TiltWebserverApplication.kt
+++ tilt-webservice/src/main/kotlin/org/heathtech/tilt/webserver/TiltWebserverApplication.kt
@@ -1,10 +1,15 @@
 package org.heathtech.tilt.webserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class TiltWebserverApplication
+class TiltWebserverApplication : SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
+        application.sources(TiltWebserverApplication::class.java)
+}
 
 fun main(args: Array<String>) {
     runApplication<TiltWebserverApplication>(*args)
